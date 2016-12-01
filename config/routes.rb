143921f1af7fa710177017a49bfa26f0364d083f
EleVers <elena_ver@bk.ru>
2016-12-01 18:34:16 +0300

    commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+#  get 'main/index'
+#  get 'main/help'
+#  get 'main/contacts'
+#  get 'main/about'
+
+  match 'index',    to: 'main#index',    via: 'get'
+  match 'help',     to: 'main#help',     via: 'get'
+  match 'contacts', to: 'main#contacts', via: 'get'
+  match 'about',    to: 'main#about',    via: 'get'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
